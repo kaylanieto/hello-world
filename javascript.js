00188fa11bc2655eb798a534e0703384537e4e95
kayla nieto <kaylanieto01@gmail.com>
2018-11-10 19:33:07 +0000

    JS Done

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("myimages");
+    //   var slides= img1,img2,img3
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    slides[slideIndex].style.display = "block";
+    setTimeout(showSlides, 1000);
+
+}
